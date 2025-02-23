7dccc5906395d8d769ff395ed43f105633e6308d
tuhin-su <tuhinsubhraghara29195@gmail.com>
2025-02-23 11:59:54 +0000

    Update login add token

--- backend/endpoints/login.php
+++ backend/endpoints/login.php
@@ -3,13 +3,14 @@
     require_once __DIR__ . '/../core/Database.php';
     
     global $router;
-    $router->add('POST', '/template', function () {
+    $router->add('POST', '/login', function () {
         $db = new Database();
         $jwt = new JwtHandler();
         $data = json_decode(file_get_contents("php://input"), true);
-        $stmt = $db->query("SELECT * FROM users WHERE email = ?", [$data['email']]);
+        $stmt = $db->query("SELECT * FROM users WHERE id = ?", [$data['id']]);
         $user = $stmt->fetch(PDO::FETCH_ASSOC);
-        if ($user && password_verify($data['password'], $user['password'])) {
+        // password_verify
+        if ($user && $data['passwd'] == $user['password']) {
             echo json_encode(["token" => $jwt->generateToken(["user_id" => $user['id']])]);
         } else {
             http_response_code(401);
