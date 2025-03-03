b08d0d161d901ed9ed8a4d5ed533a80797647d10
tuhin-su <tuhinsubhraghara29195@gmail.com>
2025-03-03 16:08:52 +0000

    ADD v1

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -3,19 +3,22 @@ import { View, Text } from "react-native";
 import { useRouter, useRootNavigationState } from "expo-router";
 import globalStorage from "./components/GlobalStorage";
 import '../global.css'
-export default function LandingScreen() {
+import React from "react";
+export default async function LandingScreen() {
   const router = useRouter();
   const navigationState = useRootNavigationState();
-  globalStorage.setTemp("pageTitle","Login")
+  globalStorage.setTemp("pageTitle","Login");
+  const token = await globalStorage.getValue("token")
   useEffect(() => {
-    if (navigationState?.key) {
+    if (!token) {
       router.push("/login");
       return;
     }
+    router.push("/home");
   }, [navigationState?.key]);
   return (
     <View className="flex-1 justify-center items-center bg-blue-500">
-      <Text className="text-white text-2xl font-bold">This application Mailfunctiond pls conntuct devloper for fix this. or contuct innogate@yhaoo.com</Text>
+      <Text className="text-white text-2xl font-bold">This app in devolvement mode application Malfunction pls contact developer for fix this. or conduct innogate@yhaoo.com</Text>
     </View>
   );
 }
