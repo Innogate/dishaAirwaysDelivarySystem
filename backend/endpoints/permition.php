49fc0d6144777d967155616e443626203de849ad
tuhin-su <tuhinsubhraghara29195@gmail.com>
2025-03-19 19:59:47 +0530

    updated acess

--- backend/endpoints/permition.php
+++ backend/endpoints/permition.php
@@ -25,6 +25,8 @@ $router->add('POST', '/master/access/userId', function () {
     (new ApiResponse(200, "Success", $list))->toJson();
 });
 
+
+
 $router->add('POST', '/master/access', function () {
     $pageID = 8;
     $jwt = new JwtHandler();
@@ -45,19 +47,46 @@ $router->add('POST', '/master/access/update', function () {
     $pageID = 8;
     $jwt = new JwtHandler();
     $handler = new Handler();
+    
+    // Validate JWT token
     $_info = $jwt->validate();
-    $_info = $jwt->validate();
-
-    $handler->validatePermission($pageID,$_info->user_id, 'u');
-    $require_field = ['user_id','permission_code','page_id'];
+    
+    // Validate user permission for the page
+    $handler->validatePermission($pageID, $_info->user_id, 'u');
+    
+    // Required fields for input validation
+    $require_field = ['user_id', 'permission_code', 'page_id'];
+    
+    // Get input data
     $data = json_decode(file_get_contents("php://input"), true);
+    
+    // Validate input data
     $handler->validateInput($data, $require_field);
+    
+    // Connect to the database
     $db = new Database();
-    $sql = "UPDATE permissions SET permission_code = ?, page_id = ? WHERE user_id = ?";
-    $stmt = $db->query($sql, [$data['permission_code'], $data['page_id'], $data['user_id']]);
-    $list = $stmt->fetchAll(PDO::FETCH_ASSOC) ?: [];
-    (new ApiResponse(200, "Success", $list))->toJson();
+    
+    // Check if the user already has access to the requested page
+    $sqlCheck = "SELECT * FROM permissions WHERE user_id = ? AND page_id = ?";
+    $stmtCheck = $db->query($sqlCheck, [$data['user_id'], $data['page_id']]);
+    $existingPermission = $stmtCheck->fetch(PDO::FETCH_ASSOC);
+    
+    if ($existingPermission) {
+        // User has access, so update the record
+        $sqlUpdate = "UPDATE permissions SET permission_code = ? WHERE user_id = ? AND page_id = ?";
+        $db->query($sqlUpdate, [$data['permission_code'], $data['user_id'], $data['page_id']]);
+        $message = "Permission updated successfully";
+    } else {
+        // User doesn't have access, so insert a new record
+        $sqlInsert = "INSERT INTO permissions (user_id, permission_code, page_id) VALUES (?, ?, ?)";
+        $db->query($sqlInsert, [$data['user_id'], $data['permission_code'], $data['page_id']]);
+        $message = "Permission granted successfully";
+    }
+
+    // Send a success response
+    (new ApiResponse(200, $message, []))->toJson();
 });
+
 
 $router->add("GET","/master/pages", function () {
     $jwt = new JwtHandler();
