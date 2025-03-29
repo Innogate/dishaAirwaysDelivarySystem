0abc8d68d619edfa7169a87fe831fcf66e57219b
tuhin-su <tuhinsubhraghara29195@gmail.com>
2025-03-29 10:49:52 +0530

    Search impliement

--- backend/services/autofill.php
+++ backend/services/autofill.php
@@ -4,26 +4,39 @@ require_once __DIR__ . '/../core/Database.php';
 require_once __DIR__ . '/../core/Handler.php';
 
 global $router;
-$router->add('GET', '/autofill/newBooking', function () {
+$router->add('POST', '/autofill/newBooking', function () {
     $jwt = new JwtHandler();
     $handler = new Handler();
     $_info = $jwt->validate();
-
+    $data = json_decode(file_get_contents("php://input"), true);
 
 
     $db = new Database();
-    $sql = "SELECT consignee_name, consignor_name, destination_city_id, destination_branch_id, COUNT(*) as entry_count
-FROM bookings
-WHERE branch_id = ? 
-GROUP BY consignee_name, consignor_name, destination_city_id, destination_branch_id
-HAVING COUNT(*) > 1
-ORDER BY entry_count DESC LIMIT 60;";
-    $stmt = $db->query($sql, [$_info->branch_id]);
-    $list = $stmt->fetchAll(PDO::FETCH_ASSOC);;
+    $search = $data["search"];
+   
+    
+    $sql = "SELECT DISTINCT ON (b.consignee_name, b.consignee_mobile, b.consignor_name, b.consignor_mobile) 
+    b.consignee_name, 
+    b.consignee_mobile, 
+    b.booking_address, 
+    b.consignor_name, 
+    b.consignor_mobile, 
+    b.destination_branch_id
+FROM public.bookings b
+WHERE 
+    b.consignee_name ILIKE ? 
+    OR CAST(b.consignee_mobile AS TEXT) ILIKE ? 
+    OR b.consignor_name ILIKE ? 
+    OR CAST(b.consignor_mobile AS TEXT) ILIKE ?
+LIMIT 30";
+    
+    $stmt = $db->query($sql, [$search, $search, $search, $search]); 
+    $list = $stmt->fetch(PDO::FETCH_ASSOC);
+    
     if (!$list) {
-        $list = [];
+        $list = NULL;
     }
-
+    
     $response = new ApiResponse(200, "Success", $list);
     $response->toJson();
 });
