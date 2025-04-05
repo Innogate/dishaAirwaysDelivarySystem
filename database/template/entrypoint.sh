cac4d6abff88cf5c6313e19720bd633e87b27d67
tuhin-su <tuhinsubhraghara29195@gmail.com>
2025-04-05 05:33:02 +0000

    DB UPDATE

--- database/template/entrypoint.sh
+++ database/template/entrypoint.sh
@@ -10,7 +10,11 @@ sleep 10
 # Restore database if backup exists
 if [ -f "/docker-entrypoint-initdb.d/test.sql" ]; then
     echo "Restoring database..."
-    psql -U $POSTGRES_USER -d $POSTGRES_DB < /docker-entrypoint-initdb.d/test.sql
+    if [ $PRODUCTION -eq true ];then
+        psql -U $POSTGRES_USER -d $POSTGRES_DB < /docker-entrypoint-initdb.d/test.sql
+    else
+        psql -U $POSTGRES_USER -d $POSTGRES_DB < /docker-entrypoint-initdb.d/mock_data.sql
+    fi
 fi
 
 # Keep the container running
